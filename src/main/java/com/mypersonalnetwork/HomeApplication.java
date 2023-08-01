c12725c7faaf4fd567624df520fa7a8c3f66f004
Federico Calò <fedcal01@gmail.com>
2023-08-01 11:32:47 +0200

    Logging system

--- src/main/java/com/mypersonalnetwork/HomeApplication.java
+++ src/main/java/com/mypersonalnetwork/HomeApplication.java
@@ -2,33 +2,39 @@ package com.mypersonalnetwork;
 
 import com.mypersonalnetwork.database.connection.DatabaseConnectionException;
 import com.mypersonalnetwork.database.connection.DbConnection;
+import com.mypersonalnetwork.logsystem.LogMain;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.logging.Level;
 
 public class HomeApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException, DatabaseConnectionException {
+        new LogMain();
 
+        LogMain.writeLog("Start programming\n", Level.INFO,HomeApplication.class.getName());
         try {
             DbConnection dbConnection = new DbConnection();
         } catch(ClassNotFoundException e) {
-            throw new DatabaseConnectionException("Attenzione!!! Driver non trovato: " + e.getMessage());
+            LogMain.writeLog(e.getClass()+" Driver Database non trovato.\n", Level.WARNING,HomeApplication.class.getName());
         } catch(InstantiationException e){
-            throw new DatabaseConnectionException("Attenzione!!! Errore durante l'instanzazione: " + e.getMessage());
+            LogMain.writeLog(e.getClass()+" Errore durante l'inizializzazione della connessione al database.\n", Level.WARNING,HomeApplication.class.getName());
         } catch(IllegalAccessException e){
-            throw new DatabaseConnectionException("Attenzione!!! Accesso al driver negato: " + e.getMessage());
+            LogMain.writeLog(e.getClass()+" Accesso al driver del database negato.\n", Level.WARNING,HomeApplication.class.getName());
         }catch(SQLException e) {
-            throw new DatabaseConnectionException("Attenzione!!! SQLException: " + e.getMessage());
+            LogMain.writeLog(e.getClass()+" Impossibile eseguire la query di connessione.\n", Level.WARNING,HomeApplication.class.getName());
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
+
+        LogMain.writeLog("Connection to Database\n",Level.INFO,HomeApplication.class.getName());
+        LogMain.writeLog("Start UI\n",Level.INFO,HomeApplication.class.getName());
 
         FXMLLoader fxmlLoader = new FXMLLoader(HomeApplication.class.getResource("home-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
---
+++ src/main/java/com/mypersonalnetwork/logsystem/LogMain.java
@@ -0,0 +1,62 @@
+package com.mypersonalnetwork.logsystem;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class LogMain {
+    private static Logger logger;
+    private static final String mainLog="mainLog.txt";
+    private static final String exceptionLog="exceptionLog.txt";
+    private static final String pathFolder="./logging/";
+
+    public LogMain() throws IOException {
+        Path mainFolder = Path.of(pathFolder);
+        if(!Files.exists(mainFolder)) {
+            Files.createDirectories(mainFolder);
+            Files.createFile(Path.of(pathFolder+mainLog));
+            Files.createFile(Path.of(pathFolder+exceptionLog));
+        }
+    }
+
+    public static final void writeLog(String msg, Level typeLog, String nameClass)throws IOException{
+        logger = Logger.getLogger(nameClass);
+        if(typeLog.equals("WARNING")){
+            writeExceptionLog(msg);
+        }else {
+            writeMainLog(msg,typeLog);
+        }
+    }
+
+    private static void writeMainLog(String msg, Level typeLog) throws IOException {
+        Stream<String> lines = Files.lines(Path.of(pathFolder+mainLog));
+        String data = lines.collect(Collectors.joining("\n"));
+        lines.close();
+        File myObj = new File(pathFolder+mainLog);
+        myObj.delete();
+        myObj.createNewFile();
+        Files.write(Path.of(pathFolder+mainLog), (LocalDateTime.now()+": "+msg).getBytes(), StandardOpenOption.APPEND);
+        Files.write(Path.of(pathFolder+mainLog),data.getBytes(), StandardOpenOption.APPEND);
+    }
+
+    private static void writeExceptionLog(String msg) throws IOException {
+        Stream<String> lines = Files.lines(Path.of(pathFolder+exceptionLog));
+        String data = lines.collect(Collectors.joining("\n"));
+        lines.close();
+        File myObj = new File(pathFolder+exceptionLog);
+        myObj.delete();
+        myObj.createNewFile();
+        Files.write(Path.of(pathFolder+exceptionLog), (LocalDateTime.now()+": "+msg).getBytes(), StandardOpenOption.APPEND);
+        Files.write(Path.of(pathFolder+exceptionLog),data.getBytes(), StandardOpenOption.APPEND);
+    }
+}
